7b43770fafa5f9527c776f2d7d13cacfdb858f60
Anastasiya Piashko <anapiashko@gmail.com>
2020-02-10 20:10:19 +0300

    add file price per one km_kg

--- calc-price/src/main/java/com/epam/brest/Main.java
+++ calc-price/src/main/java/com/epam/brest/Main.java
@@ -1,6 +1,7 @@
 package com.epam.brest;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,12 +11,17 @@ public class Main {
         String path_to_resource = "src/main/resources";
         String file_path_distance = path_to_resource + "/coefficients_distance_km";
         String file_path_weight = path_to_resource + "/coefficients_weight_kg";
+        String file_path_price_per_one = path_to_resource + "/price_per_one_kmKg";
 
         File file = new File();
 
         Coefficients distance = new Coefficients(file.readFromFile(file_path_distance));
 
         Coefficients weight = new Coefficients(file.readFromFile(file_path_weight));
+
+        List<String> price_per_one_kmKg = file.readFromFile(file_path_price_per_one);
+        BigDecimal price_per_oneKm = new BigDecimal(price_per_one_kmKg.get(0));
+        BigDecimal price_per_oneKg = new BigDecimal(price_per_one_kmKg.get(1));
 
         BigDecimal[] enteredValues = new BigDecimal[4];
 
@@ -27,12 +33,8 @@ public class Main {
         do {
             if (i == 0) {
                 System.out.println("Please enter distance or Q for exit : ");
-            } else if (i == 1) {
-                System.out.println("Please enter price per km or Q for exit : ");
             } else if (i == 2) {
                 System.out.println("Please enter weight or Q for exit : ");
-            } else {
-                System.out.println("Please enter price per kg or Q for exit : ");
             }
 
             inputValue = scanner.next();
@@ -44,22 +46,22 @@ public class Main {
                 }
             }
 
-            if(i == 2) {
-                enteredValues[1] = enteredValues[1]
-                        .multiply(distance.getCoefficientByValue(enteredValues[0]));
+            if(i == 1) {
+                enteredValues[1] = price_per_oneKm.multiply(distance.getCoefficientByValue(enteredValues[0]));
+                i++;
             }
-            if(i == 4){
-                enteredValues[3] = enteredValues[3]
-                        .multiply(weight.getCoefficientByValue(enteredValues[2]));;
+            if(i == 3){
+                enteredValues[3] = price_per_oneKg.multiply(weight.getCoefficientByValue(enteredValues[2]));;
             }
 
-            if (i == 4) {
+            if (i == 3) {
                 BigDecimal calcResult = enteredValues[0].multiply(enteredValues[1])
                         .add
                                 (enteredValues[2].multiply(enteredValues[3]));
                 System.out.println("Price $ " + calcResult);
                 i = 0;
             }
+
         } while (!isExitValue(inputValue));
 
         System.out.println("Finish!");
